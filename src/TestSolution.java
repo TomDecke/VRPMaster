e7f1f6151b727f4d54a92a10291f4d645bfdf3ca
Tom Decke <2354160d@student.gla.ac.uk>
2018-07-11 11:06:11 +0100

    verified test and added no-customer-constraint

--- src/TestSolution.java
+++ src/TestSolution.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -16,13 +17,13 @@ public class TestSolution {
 	 * @return boolean, whether or not the solution is valid
 	 */
 	public static boolean runTest(VRP vrpIn, double solDistIn, ArrayList<Vehicle> vehicles) {
-		
+
 		//create variables for the input
 		VRP vrp = vrpIn;
 		double solDist = solDistIn;
 		ArrayList<Vehicle> solVehicles = vehicles;
 		int[] cVisited = new int[vrp.customer.length] ;		
-		
+
 		//distance of all vehicles
 		double totalDist = 0; 
 
@@ -46,12 +47,18 @@ public class TestSolution {
 
 			//Check the route of the vehicle
 			while(!cCur.equals(v.lastCustomer)) {
-				
+
 				//check if the customer actually is in the vehicle
 				if(!v.equals(cCur.vehicle)) {
 					System.out.println(cCur);
 					System.out.println(v);
 					System.out.println("Customer in wrong vehicle");
+					return false;
+				}
+				
+				//check if the target of travel belongs to the vrp
+				if(cSucc.custNo > vrp.n) {
+					System.out.println("Customer does not belong to the VRP.");
 					return false;
 				}
 
@@ -109,7 +116,7 @@ public class TestSolution {
 		//check if all customers have been visited exactly once
 		for (int i = 0; i < cVisited.length ; i++) {
 			int visits = cVisited[i];
-			
+
 			if(visits == 0) {
 				System.out.println("Did not visit all customers");
 				return false;
@@ -131,5 +138,70 @@ public class TestSolution {
 		System.out.println("The solution is valid!");
 		return true;
 	}
+
+	public static void main(String[] args) throws IOException {
+
+		//get the input
+		String fileIn = args[0];
+		int numCustomer = Integer.parseInt(args[1]);
+
+		//create verification instance and solver
+		VRP vrp = new VRP(fileIn,numCustomer);
+
+		System.out.print(String.format("%s", "\\|"));
+		for(Customer c : vrp.customer) {
+			System.out.print(String.format("%5d|", c.custNo));
+		}
+		System.out.println("");
+		for(Customer c : vrp.customer) {
+			System.out.print(String.format("%d|", c.custNo));
+			for(Customer c2 : vrp.customer) {
+				System.out.print(String.format("%5.2f|", vrp.distance(c,c2)));
+			}	
+			System.out.println("");
+		}
+		
+		Customer c1 = vrp.customer[1];
+		Customer c2 = vrp.customer[2];
+		Customer c3 = vrp.customer[3];
+		Customer c4 = vrp.customer[4];
+		Customer c5 = vrp.customer[5];
+		
+		Vehicle v = vrp.vehicle[4];
+		Vehicle v2 = vrp.vehicle[2];
+		System.out.println(v.id);
+		System.out.println(c5.toString());
+		
+		Customer lastV1 = v.lastCustomer;
+		Customer lastV2 = v2.lastCustomer;
+		
+		Customer cX = new Customer(6,48,48, 20,666,666,5);
+		cX.earliestStart = 666;
+		cX.latestStart = 666;
+		
+		//fill v1
+		v.insertBetween(c4, c5, lastV1);
+		v.insertBetween(c1, c4, lastV1);
+		v.show();
+		
+		//fill v2
+		cX.vehicle = v2;
+		cX.succ = lastV2;
+		lastV2.pred = cX;
+		c3.succ = cX;
+		cX.pred = c3;
+//		v2.insertBetween(cX, c3, lastV2);
+//		v2.insertBetween(c1, c4, lastV2);
+//		v2.insertBetween(c4, c1, lastV2);
+		v2.show();
+	
+		
+		ArrayList<Vehicle> testV = new ArrayList<Vehicle>();
+		testV.add(v);
+		testV.add(v2);
+		testV.add(vrp.vehicle[1]);
+		TestSolution.runTest(vrp, v.cost+73.4800872493708, testV);
+	}
+
 
 }
