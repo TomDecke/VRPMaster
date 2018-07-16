80a36b4fe248a97e45c25c259eb3fecf3be90bdf
Tom Decke <2354160d@student.gla.ac.uk>
2018-07-16 15:18:15 +0100

    improved accessing results

--- src/RunDescents.java
+++ src/RunDescents.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 /**
- * Class to obtain results from multiple input files
+ * Class to obtain results from multiple input files in a given directory
  * @author Tom Decke
  *
  */
@@ -9,9 +9,8 @@ public class RunDescents {
 
 	public static void main(String[] args) throws IOException {
 		String folderpath = args[0];
-		String resultpath = args[1];
-		int numCustomers = Integer.parseInt(args[2]);
-		int mode = Integer.parseInt(args[3]);
+		String resultpath = folderpath+"results\\";
+		int numCustomers = Integer.parseInt(args[1]);
 		
 		SteepestDescent stDesc = null;
 		VRP vrp = null; 
@@ -19,14 +18,28 @@ public class RunDescents {
 		File folder = new File(folderpath);
 		File[] listOfFiles = folder.listFiles();
 
+		File result = new File(resultpath);
+		result.mkdir();
+		
+		FileWriter writer = new FileWriter(resultpath+"soln_"+numCustomers+".txt");
+		
 		for (File file : listOfFiles) {
 		    if (file.isFile()) {
-		    	String vrpInstance = folderpath +file.getName(); 
-		        System.out.println(vrpInstance);
+		    	writer.write(file.getName()+"\n");
+		    	String vrpInstance = folderpath +file.getName();
 		        vrp = new VRP(vrpInstance, numCustomers);
-		        stDesc = new SteepestDescent(vrp, resultpath+file.getName());
-		        stDesc.solve(mode);
+		        for(int i = 0; i < 5; i++) {
+		        	writer.write("mode " + i + ": ");
+			        stDesc = new SteepestDescent(vrp, resultpath + "mode_" + i + "_"+  file.getName());
+			        stDesc.solve(i);
+			        System.out.println(String.format("c: %.1f nV: %d ", stDesc.getTotalCost(),stDesc.getVehicleCount()));
+			        writer.write(String.format("cost: %.1f needed Vehicles: %d%n", stDesc.getTotalCost(),stDesc.getVehicleCount()));
+		        }
+		        writer.write("\n");
+
 		    }
 		}
+		
+		writer.close();
 	}
 }
