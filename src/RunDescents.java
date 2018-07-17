c5a0adfd241e8220e049d48efb9c1fbaffb3f8e0
Tom Decke <2354160d@student.gla.ac.uk>
2018-07-17 08:30:13 +0100

    added firstFit and comments

--- src/RunDescents.java
+++ src/RunDescents.java
@@ -8,39 +8,58 @@ import java.io.*;
 public class RunDescents {
 
 	public static void main(String[] args) throws IOException {
+		//get information from the input
 		String folderpath = args[0];
 		String resultpath = folderpath+"results\\";
 		int numCustomers = Integer.parseInt(args[1]);
-		
-		SteepestDescent stDesc = null;
-		VRP vrp = null; 
-		
-		File folder = new File(folderpath);
-		File[] listOfFiles = folder.listFiles();
+		boolean steepest = Boolean.getBoolean(args[2]);
 
+		//set up the descent and the problem instance
+		Descent desc = null;
+		VRP vrp = null; 
+
+		//create a folder for the results
 		File result = new File(resultpath);
 		result.mkdir();
 		
+		//get the files in the target directory
+		File folder = new File(folderpath);
+		File[] listOfFiles = folder.listFiles();
+
+		//set up the output-file
 		FileWriter writer = new FileWriter(resultpath+"soln_"+numCustomers+".txt");
-		
+
+		//go through the files in the directory
 		for (File file : listOfFiles) {
-		    if (file.isFile()) {
-		    	writer.write(file.getName()+"\n");
-		    	String vrpInstance = folderpath +file.getName();
+			if (file.isFile()) {
+				
+				writer.write(file.getName()+"\n");
+				String vrpInstance = folderpath +file.getName();
 
-		        for(int i = 0; i < 5; i++) {
-		        	writer.write("mode " + i + ": ");
-			        vrp = new VRP(vrpInstance, numCustomers);
-			        stDesc = new SteepestDescent(vrp, resultpath + "mode_" + i + "_"+  file.getName());
-			        stDesc.solve(i);
-			        System.out.println(String.format("c: %.1f nV: %d ", stDesc.getTotalCost(),stDesc.getVehicleCount()));
-			        writer.write(String.format("cost: %.1f needed Vehicles: %d%n", stDesc.getTotalCost(),stDesc.getVehicleCount()));
-		        }
-		        writer.write("\n");
-
-		    }
+				//execute every mode for steepest descent
+				for(int i = 0; i < 5; i++) {
+					
+					vrp = new VRP(vrpInstance, numCustomers);
+					//get the descent specified by the input
+					if(steepest) {
+						desc = new SteepestDescent(vrp, resultpath + "mode_" + i + "_"+  file.getName());
+					}
+					else {
+						desc = new FirstFitDescent(vrp, resultpath + "mode_" + i + "_"+  file.getName());
+					}
+					//make sure that first descent only executes once, if chosen
+					if(steepest || i%5==0) {
+						//solve the VRP-instance
+						desc.solve(i);
+						//write the results to the output file
+						writer.write("mode " + i + ": ");
+						System.out.println(String.format("c: %.1f nV: %d ", desc.getTotalCost(),desc.getVehicleCount()));
+						writer.write(String.format("cost: %.1f needed Vehicles: %d%n", desc.getTotalCost(),desc.getVehicleCount()));
+					}
+				}
+				writer.write("\n");
+			}
 		}
-		
 		writer.close();
 	}
 }
